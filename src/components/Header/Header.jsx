f287ffbf3a39de81fcfae5de0d5f2313936568c8
Egor Fomin <egorfomin419@gmail.com>
2026-07-05 21:35:45 +0300

    solve

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import './Header.css';
-import Navigation from '../Navigation/Navigation';
+import Navigation from '../navigation/navigation';
 
 export default function Header() {
   return (
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,4 +1,4 @@
-import './Navigation.css';
+import './navigation.css';
 
 export default function Navigation() {
   return (
